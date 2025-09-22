1ec44785a635d0558fc42257e3bc25303440e654
Sahil01dotcom <fulsundarsahil06@gmail.com>
2025-09-22 17:55:11 +0530

    Add files via upload

---
+++ Quiz.jsx
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from 'react';
+import "./Quiz.css";
+import{ data } from '../../assets/data';
+
+const Quiz = () => {
+
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock, setLock] = useState(false);// to lock the options after one is selected
+    let [score, setScore] = useState(0);
+    let [result, setResult] = useState(false);
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let Option_Array = [Option1, Option2, Option3, Option4];
+
+
+    const checkAns = (e,ans) => {
+        if (lock ===false){
+        if (question.ans === ans) {
+            e.target.classList.add("correct");
+            setLock(true);
+            setScore(prev=>prev+1);
+        } 
+        else {
+            e.target.classList.add("incorrect");
+            setLock(true);
+            Option_Array[question.ans-1].current.classList.add("correct");
+        }
+    }
+    }
+
+    const Next = () => {
+        if (lock==true){
+            if(index === data.length - 1){
+                setResult(true);
+                return 0;   
+            }
+
+
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            Option_Array.map ((option)=>{
+                option.current.classList.remove("correct");
+                option.current.classList.remove("incorrect");
+                return null;
+            });
+        }
+    } 
+
+    const reset = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setLock(false);
+        setScore(0);
+        setResult(false);
+       
+    }
+
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        {result?<></>:<>
+        <h2>{index + 1}. {question.question}</h2>
+        <ul>
+            <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+        </ul>
+        <button onClick={Next}>Next</button>
+        <div className="index">{index + 1} of {data.length} questions</div></>}
+        {result?<>
+        <h2>Your Score: {score} out of {data.length}</h2>
+        <button onClick={reset}>Reset</button>
+        </>:<></>}
+         
+    </div> 
+        
+  ); 
+}
+
+export default Quiz;   
